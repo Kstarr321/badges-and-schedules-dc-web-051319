7876814c7f75ec249cd24fb31d9ac41163a30560
Kolton Starr <starrkolton@gmail.com>
2019-04-29 18:44:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(arr)
+  arr.collect do |speaker|
+    badge_maker(speaker)
+  end 
+end 
